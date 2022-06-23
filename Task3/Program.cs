5694d8e05b102378b5df5c5b9d8aecf09a9c2282
AndreyVolkov <volk_bur-gora@mail.ru>
2022-06-23 21:56:08 +0300

    задача 3

---
+++ Task3/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, 
+//что третьей цифры нет.
+int prompt(string message)
+{
+Console.WriteLine(message);
+string param = Console.ReadLine(); 
+int a = int.Parse(param); 
+return a;
+}
+int a = prompt("введите число");
+if (a<100)
+{
+    System.Console.WriteLine("не трехзначное число");
+}  
+else
+{
+    while (a>1000)
+    {
+    a = a / 10;
+    }
+    a = a % 10;
+    System.Console.WriteLine("Третья цифра числа: " + a);
+}
