8f1a04478e7f8fc6574126b6c5be6c80557303c7
AndreyVolkov <volk_bur-gora@mail.ru>
2022-06-23 21:55:04 +0300

    задача 1

---
+++ Task1/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+
+Console.WriteLine("Введите трехзначное число: ");
+string param = Console.ReadLine(); 
+int N = int.Parse(param); 
+if ((N>=99) && (N<=999))
+{
+    System.Console.WriteLine(N%10);
+}
+else {
+    System.Console.WriteLine("введено не трехзначное число");
+}
